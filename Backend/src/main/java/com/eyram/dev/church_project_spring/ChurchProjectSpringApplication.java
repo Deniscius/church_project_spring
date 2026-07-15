4db7165f2b6793325145484ba91d17906589a51e
ADJEVI Koffi Eyram Dénis <125583111+Deniscius@users.noreply.github.com>
2026-07-15 12:02:13 +0000

    chore(jpa): enable entity auditing

--- Backend/src/main/java/com/eyram/dev/church_project_spring/ChurchProjectSpringApplication.java
+++ Backend/src/main/java/com/eyram/dev/church_project_spring/ChurchProjectSpringApplication.java
@@ -4,13 +4,14 @@ import com.eyram.dev.church_project_spring.config.AdminSeedProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 @EnableConfigurationProperties(AdminSeedProperties.class)
 public class ChurchProjectSpringApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ChurchProjectSpringApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ChurchProjectSpringApplication.class, args);
+    }
 }

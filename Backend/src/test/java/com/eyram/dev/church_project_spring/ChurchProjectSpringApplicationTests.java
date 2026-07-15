2a71677cd249d59ecef0140a0d31fa47a6b38bb0
ADJEVI Koffi Eyram Dénis <125583111+Deniscius@users.noreply.github.com>
2026-07-15 13:03:05 +0000

    test(config): run application context with isolated test profile

--- Backend/src/test/java/com/eyram/dev/church_project_spring/ChurchProjectSpringApplicationTests.java
+++ Backend/src/test/java/com/eyram/dev/church_project_spring/ChurchProjectSpringApplicationTests.java
@@ -2,12 +2,13 @@ package com.eyram.dev.church_project_spring;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ChurchProjectSpringApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }

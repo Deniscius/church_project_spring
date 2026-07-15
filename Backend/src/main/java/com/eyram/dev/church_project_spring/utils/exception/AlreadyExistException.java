21dca2012a8d10c52e70d4c3042f895f7c8e14be
ADJEVI Koffi Eyram Dénis <125583111+Deniscius@users.noreply.github.com>
2026-07-15 13:44:09 +0000

    error-handling: align duplicate resource status with conflict semantics

--- Backend/src/main/java/com/eyram/dev/church_project_spring/utils/exception/AlreadyExistException.java
+++ Backend/src/main/java/com/eyram/dev/church_project_spring/utils/exception/AlreadyExistException.java
@@ -3,7 +3,7 @@ package com.eyram.dev.church_project_spring.utils.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.BAD_REQUEST)
+@ResponseStatus(value = HttpStatus.CONFLICT)
 public class AlreadyExistException extends RuntimeException {
     public AlreadyExistException(String message) {
         super(message);

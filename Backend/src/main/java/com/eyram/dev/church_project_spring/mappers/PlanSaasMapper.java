6cd54fd99ba9ea6af5135a0337ccab36a5f12f1d
ADJEVI Koffi Eyram Dénis <125583111+Deniscius@users.noreply.github.com>
2026-08-17 14:54:17 +0000

    Add SaaS pricing plan mapper

---
+++ Backend/src/main/java/com/eyram/dev/church_project_spring/mappers/PlanSaasMapper.java
@@ -0,0 +1,25 @@
+package com.eyram.dev.church_project_spring.mappers;
+
+import com.eyram.dev.church_project_spring.DTO.request.PlanSaasRequest;
+import com.eyram.dev.church_project_spring.DTO.response.PlanSaasResponse;
+import com.eyram.dev.church_project_spring.entities.PlanSaas;
+import org.mapstruct.BeanMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.NullValuePropertyMappingStrategy;
+
+@Mapper(componentModel = "spring")
+public interface PlanSaasMapper {
+
+    PlanSaasResponse modelToDto(PlanSaas entity);
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "publicId", ignore = true)
+    @Mapping(target = "code", ignore = true)
+    @Mapping(target = "statusDel", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    void updateEntityFromDto(PlanSaasRequest request, @MappingTarget PlanSaas entity);
+}
